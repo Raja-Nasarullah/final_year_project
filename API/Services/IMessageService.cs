1afadea5f0b4db7724ff76631ca2bfdaa3f09449
Raja-Nasarullah <221049@students.au.edu.pk>
2025-11-14 00:02:05 +0500

    Nasir (#1)
    * adding new readme
    
    * Adding Final year project

---
+++ API/Services/IMessageService.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public interface IMessageService
+    {
+        Task<List<Message>> GetAllAsync();
+        Task<Message?> GetByIdAsync(string id);
+        Task CreateAsync(Message message);
+        Task UpdateAsync(string id, Message updatedMessage);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ Model/Message.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace Model
+{
+    public class Message
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string? Id { get; set; }       // Unique message ID
+        public string? SenderId { get; set; } = "";
+        public string? SenderName { get; set; } = "";
+        public string? Text { get; set; } = "";
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
